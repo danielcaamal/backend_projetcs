9415ecfaf919ac960a415f656e214b50d46d8451
daniel.caamal <danielcaamal97@gmail.com>
2022-04-29 12:19:47 -0500

    Project base commit

---
+++ django_rest_framework/watchmate/watchmate/watchlist_app/models.py
@@ -0,0 +1,40 @@
+# Django
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# StreamPlatform Model
+class StreamPlatform(models.Model):
+    name = models.CharField(max_length=30)
+    about = models.CharField(max_length=150)
+    website = models.URLField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+
+# Watchlist model
+class Watchlist(models.Model):
+    title = models.CharField(max_length=50)
+    storyline = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name='watchlist')
+    
+    def __str__(self):
+        return self.title
+
+
+# Review model
+class Review(models.Model):
+    review_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.CharField(max_length=200, null=True)
+    watchlist = models.ForeignKey(Watchlist, on_delete=models.CASCADE, related_name='reviews')
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.rating} - {self.description}"
---
+++ django_rest_framework/watchmate/watchmate/watchmate/urls.py
@@ -0,0 +1,9 @@
+# Django
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('watchlist_app.api.urls')),
+]
