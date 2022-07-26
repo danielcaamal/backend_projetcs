a3299d622bdf4a2450060e9bbde8232614c46275
daniel.caamal <danielcaamal97@gmail.com>
2022-07-26 12:39:27 -0500

    initial commit

---
+++ nodejs/microservicios/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    api: {
+        port: process.env.API_PORT || 3000,
+    },
+    post: {
+        port: process.env.POST_PORT || 3002,
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET || 'notasecret!',
+    },
+    mysql: {
+        host: process.env.MYSQL_HOST || 'remotemysql.com',
+        user: process.env.MYSQL_USER || 'vyU7DCqzKX',
+        password: process.env.MYSQL_PASS || 'hQ8gtxIQ9o',
+        database: process.env.MYSQL_DB || 'vyU7DCqzKX',
+    },
+    mysqlService: {
+        host: process.env.MYSQL_SRV_HOST || 'localhost',
+        port: process.env.MYSQL_SRV_PORT || 3001,
+    }
+}
